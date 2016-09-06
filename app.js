0c312e615641ce052b37eab8c38a2e1fbf23ad3a
Damian <damian>
2016-09-05 22:02:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+(function (){
+console.log('Hola Mundo');
+
+
+//variables array
+var lista = [];
+
+lista.push('Hola');
+lista.push('Mundo');
+
+
+console.log(lista);
+
+//variables tipo String
+var str= 'Hola';
+
+//variables tipo numerico
+var numerico= 120;
+
+//variable del tipo object
+var object ={};
+
+object.descripcion ='Naranjas';
+object.cantidad = 10;
+console.log(object);
+
+
+}());
